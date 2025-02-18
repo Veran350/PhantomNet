a011b77e51cd92828ac7552bd8a6f980964c513a
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-19 00:27:28 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,9 @@
-#!/data/data/com.termux/files/usr/bin/bash  
-termux-change-repo <<< $'1\n1\n'  
-pkg update -y  
-pkg install -y git nodejs python php dnsmasq android-tools  
-npm install express  
-chmod +x ../core/*.sh ../scripts/*.sh ../core/persistence/*  
-termux-setup-storage  
+#!/data/data/com.termux/files/usr/bin/bash
+termux-setup-storage
+termux-change-repo <<< $'1\n1\n'
+pkg update -y
+pkg install -y git nodejs python php dnsmasq android-tools
+npm install express@4.18.2
+mkdir -p core/persistence implants
+chmod 777 core/*.sh scripts/*.sh core/persistence/*
+termux-fix-shebang scripts/*.sh
