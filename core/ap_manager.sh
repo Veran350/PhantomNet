c0ec8b84ce73e9d099386701782bf14c84376d37
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-18 23:41:55 +0100

    Delete core directory

--- core/ap_manager.sh
+++
@@ -1,6 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash  
-termux-wifi-enable true  
-termux-wifi-connection "FreePremiumWiFi"  
-ifconfig wlan0 192.168.1.1 netmask 255.255.255.0  
-dnsmasq -d -C <(echo -e "interface=wlan0\ndhcp-range=192.168.1.100,192.168.1.200,12h")  
-php -S 192.168.1.1:8080 -t ./core/payloads &  
